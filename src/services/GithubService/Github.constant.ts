2218cdaab567ff48bb840974d90902c1acb3a83a
Lucas Figueiredo <lucas.figueiredo@engenharia.ufjf.br>
2023-04-21 15:15:34 -0300

    feat: creates boilerplate pages

---
+++ src/services/GithubService/Github.constant.ts
@@ -0,0 +1,13 @@
+export const GithubRoutes = {} as const;
+
+export const GithubLanguages = {
+  JavaScript: "JavaScript",
+  Swift: "Swift",
+  Go: "Go",
+  CSS: "CSS",
+  TypeScript: "TypeScript",
+  Java: "Java",
+  "C++": "C++",
+  Dockerfile: "Dockerfile",
+  Shell: "Shell",
+} as const;
