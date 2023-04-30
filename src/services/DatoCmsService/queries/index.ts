0198a250f6bf19e23f15b4406227457297d2212a
Lucas Figueiredo <lucas.figueiredo@engenharia.ufjf.br>
2023-04-30 11:30:47 -0300

    feat: changes allExperiences query to DESC

--- src/services/DatoCmsService/queries/index.ts
+++ src/services/DatoCmsService/queries/index.ts
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 export const GetExperiences = gql`
   query {
-    allExperiences(orderBy: endDate_ASC) {
+    allExperiences(orderBy: endDate_DESC) {
       company
       startDate
       endDate
