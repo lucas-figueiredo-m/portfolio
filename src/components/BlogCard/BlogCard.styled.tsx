0b18dc04eada4e5ad45e652cd78c84bbe0d5e523
Lucas Figueiredo <lucas.figueiredo@engenharia.ufjf.br>
2023-07-01 12:19:24 -0300

    feat: create basic blog post card component

--- src/components/BlogCard/BlogCard.styled.tsx
+++ src/components/BlogCard/BlogCard.styled.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const BlogCardContainer = styled.div`
   display: flex;
-  /* width: 100%; */
+  width: 100%;
   height: 20rem;
   background-color: blue;
 `;
